f1f8ec371e12232ff6a39d3119fab1e3e8f95a97
Giovanni Capeli <capeli.andreas@gmail.com>
2023-02-14 13:02:58 +0100

    running game

--- main.py
+++ main.py
@@ -2,13 +2,37 @@ import turtle
 import pandas
 
 s = turtle.Screen()
-s.bgpic('blank_states_img.gif')
 s.title("US States name gme")
+# s.bgpic('blank_states_img.gif')
+s.addshape('blank_states_img.gif')
+
+ts = turtle.Turtle()
+ts.shape('blank_states_img.gif')
 
 states_frame = pandas.read_csv('50_states.csv')
+states_dict = states_frame.to_dict()
+states_list = states_frame["state"].tolist()
 # print(states_frame)
 
-for n in states_frame:
-    print(n)
+guessed_states = []
 
-s.exitonclick()
+while len(guessed_states) < 50:    
+    guess = s.textinput(title='Guess', prompt='Guess a US State:').capitalize()
+
+    # print(data_with_pandas[data_with_pandas["temp"] == data_with_pandas["temp"].max()]) #it will print the item in a position that returns True
+    # check = states_frame[states_frame["state"] == guess]
+
+    if guess in states_list:
+        print("Right")
+        found_state = states_frame[states_frame["state"] == guess]
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        t.goto((int(found_state['x']) , int(found_state['y'])))
+        t.write(found_state['state'].item()) #it pick the item, or value, instead of the object of pandas
+        guessed_states.append(guess)
+    else:
+        print("Wrong")
+
+turtle.mainloop()
+# s.exitonclick()
